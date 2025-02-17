cf8e3ddce1a631afbff6983e4721bbf0a0158dcc
emrxv <esunar@wpi.edu>
2025-02-17 12:47:19 -0500

    npm initialized

---
+++ tournament-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <div className="h-screen flex items-center justify-center bg-blue-500 text-white text-3xl font-bold">
+      </div>
+  )
+}
+
+export default App
